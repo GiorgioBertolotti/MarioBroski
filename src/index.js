c65d6990ea7a197e63018631f1cf569cd933e217
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-02-21 17:51:01 +0100

    :sparkles: Added random clouds

--- src/index.js
+++ src/index.js
@@ -32,16 +32,27 @@ function loadTiles(img, ev) {
   renderFrame();
 }
 
-let testicoloPosition = { x: 100, y: 75 }
+const ball = { x: 100, y: 75 }
+const maxClouds = 4;
+const cloudsNumber = Math.floor(Math.random() * maxClouds) + 1;
+const chunkWidth = (SCREEN_WIDTH / cloudsNumber);
+const clouds = new Array(cloudsNumber).fill(0)
+  .map((_, index) => ({
+    x: (Math.floor(Math.random() * chunkWidth)) + (chunkWidth * index),
+    y: (Math.floor(Math.random() * (playgroundHeight / 2))) + BLACK_BAND_HEIGHT,
+    type: Math.floor(Math.random() * 4),
+    size: Math.floor(Math.random() * 2) + 2
+  }));
 function renderFrame() {
   context.clearRect(0, BLACK_BAND_HEIGHT, playgroundWidth, playgroundHeight);
   drawBackground();
   drawGround();
+  drawClouds();
   context.beginPath();
-  context.arc(testicoloPosition.x, testicoloPosition.y + BLACK_BAND_HEIGHT, 50, 0, 2 * Math.PI);
+  context.arc(ball.x, ball.y + BLACK_BAND_HEIGHT, 50, 0, 2 * Math.PI);
   context.stroke();
-  testicoloPosition.x += Math.random() * 20 - 10;
-  testicoloPosition.y += Math.random() * 20 - 10;
+  ball.x += Math.random() * 20 - 10;
+  ball.y += Math.random() * 20 - 10;
 
   requestAnimationFrame(renderFrame);
 }
@@ -53,8 +64,20 @@ function drawBackground() {
 
 function drawGround() {
   const y = BLACK_BAND_HEIGHT + playgroundHeight - 16;
-  const numTiles = Math.floor(SCREEN_HEIGHT / 16);
+  const numTiles = Math.floor(SCREEN_WIDTH / 16);
   for (let i = 0; i < numTiles; i++) {
     tiles[29].draw(context, i * 16, y);
   }
+}
+
+function drawClouds() {
+  clouds.forEach((cloudPos) => {
+    tiles[231 + (cloudPos.type * 26)].draw(context, cloudPos.x, cloudPos.y);
+    if (cloudPos.size > 2)
+      tiles[232 + (cloudPos.type * 26)].draw(context, cloudPos.x + 16, cloudPos.y);
+    if (cloudPos.size > 2)
+      tiles[233 + (cloudPos.type * 26)].draw(context, cloudPos.x + 32, cloudPos.y);
+    else
+      tiles[233 + (cloudPos.type * 26)].draw(context, cloudPos.x + 16, cloudPos.y);
+  })
 }
