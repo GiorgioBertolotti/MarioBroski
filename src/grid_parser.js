d6a6b22c4ede6ce45d1a393900419b5cf4c0ed9b
colobwoy <turbosmi3000@gmail.com>
2020-04-24 16:21:29 +0200

    completed world parsing

--- src/grid_parser.js
+++ src/grid_parser.js
@@ -3,22 +3,51 @@ import { BLOCKS } from "./world.js";
 export default async function parseGrid(jsonPath) {
     const json = await fetch(jsonPath)
         .then(result => result.json());
-
+    const structsMap = {};
+    for(const struct of json.structures) {
+        structsMap[struct.id] = struct;
+    }
     return json.grid.map(specs => {
         let line = [];
-        for (const spec of specs) {
-            if ("reference" in spec) {
-
-            } else {
-                const { block, quantity = 1, offset = 0 } = spec;
-                for (let i = 0; i < offset; i += 1)
+        for(const spec of specs) {
+            if("reference" in spec) {
+                const reference = structsMap[spec.reference];
+                
+                // offseting the all structure
+                const offset = spec.offset || 0;
+                for(let i = 0; i < offset; i += 1)
                     line.push(null);
-                if (block in BLOCKS) {
-                    for (let i = 0; i < quantity; i += 1)
-                        line.push(BLOCKS[block]);
-                }
+                
+                // create custom alias for this group of block
+                const aliases = {...reference, ...spec};
+                for(const block of reference.grid)
+                    addBlockGroup({...block}, line, aliases);
+            } else {
+                addBlockGroup(spec, line);
             }
         }
         return line;
     });
+}
+
+function addBlockGroup(spec, line, aliases = {}) {
+    // resolve aliases
+    for(const param in spec) {
+        const value = spec[param];
+        if (typeof(value) === "string" && value.startsWith("$")) {
+            const varName = value.substr(1);
+            if (!(varName in aliases))
+                continue;
+            spec[param] = aliases[varName]
+        }
+    }
+    // add block
+    const {block, quantity = 1, offset = 0} = spec;
+    for(let i = 0; i < offset; i += 1)
+        line.push(null);
+    if(block in BLOCKS) {
+        for(let i = 0; i < quantity; i += 1)
+            line.push(BLOCKS[block]);
+    }
+
 }
